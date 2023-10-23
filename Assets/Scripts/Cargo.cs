228ae32932b4bb6b43c91530c46017c365e30aee
Abdullah Akyol <abdullah35akyol@gmail.com>
2023-10-23 10:56:31 +0300

    Models Added + Bugs Fixed

--- Assets/Scripts/Cargo.cs
+++ Assets/Scripts/Cargo.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Dreamteck.Splines;
 using UnityEngine;
+using DG.Tweening;
 
 public class Cargo : MonoBehaviour
 {
@@ -27,8 +28,19 @@ public class Cargo : MonoBehaviour
         if (other.CompareTag("Cargo"))
         {
             _splineFollower.follow = false;
-            _splineFollower.followSpeed = -5f;
-            _splineFollower.follow = true;
+            StartCoroutine(TurnBack(other.gameObject));
         }
+    }
+
+    private IEnumerator TurnBack(GameObject other)
+    {
+        transform.DOShakeRotation(.3f, 100, 1, 1);
+        other.transform.DOShakeRotation(.3f, 100, 1, 1);
+        
+        yield return new WaitForSeconds(.5f);
+        
+        _splineFollower.followSpeed = -5f;
+        _splineFollower.follow = true;
+
     }
 }
